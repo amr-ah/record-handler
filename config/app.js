a72109ea02407c2fc91022f1a0a0c28add362d65
amr <amro.ah@hotmail.com>
2021-01-17 01:32:37 +0300

    fix: try to fix port issue

--- config/app.js
+++ config/app.js
@@ -3,6 +3,6 @@ let environment = process.env;
 
 module.exports = {
     nodeEnv: environment.NODE_ENV,
-    appPort: parseInt(environment.APP_PORT) || 3000,
+    appPort: parseInt(process.env.PORT) || 3000,
     dbUrl: environment.DB_URL,
 };
